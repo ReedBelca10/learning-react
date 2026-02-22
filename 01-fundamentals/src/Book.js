66137549eed674d74fbb0262b7dfeb8738fd1d18
ReedBelca <reedbelca55@gmail.com>
2026-02-22 10:51:37 +0000

    first commit

---
+++ 01-fundamentals/src/Book.js
@@ -0,0 +1,17 @@
+const Book = ({ img, title, author, getBook, id, number }) => {
+  // const getSingleBook = () => {
+  //   getBook(id);
+  // };
+
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <button onClick={() => getBook(id)}>reference me</button>
+          <h4>{author}</h4>
+          <span className="number">{`# ${number + 1}`}</span>
+    </article>
+  );
+};
+
+export default Book;
---
+++ 01-fundamentals/src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+import { books } from "./books.js";
+import Book from "./Book.js";
+
+function BookList() {
+  const getBook = (id) => {
+    const book = books.find((book) => book.id === id);
+    console.log(book);
+  };
+  return (
+    <>
+    <h1>Amazon Best Sellers</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          return (
+            <Book {...book} key={book.id} getBook={getBook} number={index} />
+          );
+        })}
+      </section>
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
